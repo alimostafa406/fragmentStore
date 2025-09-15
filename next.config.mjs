d12edce84b987ca7df6290d3cd9c608ef233f536
Ali Mostafa <alimostafa406@gmail.com>
2025-09-15 13:34:47 +0300

    Initial commit - Next.js store

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      i18n: {
-    locales: ['en', 'ar'], // اللغات المدعومة
-    defaultLocale: 'en',   // اللغة الافتراضية
-  },
+
 };
 
 export default nextConfig;
